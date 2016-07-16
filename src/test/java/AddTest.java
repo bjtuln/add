dcba6afca3cfe7c5daa182ea193d5e38420874ea
bjtuln <1547272811@qq.com>
2016-07-16 16:03:59 +0800

    third

--- src/test/java/AddTest.java
+++ src/test/java/AddTest.java
@@ -1,7 +1,8 @@
 import org.junit.Assert;
+import org.junit.Test;
 
-public class  Test{
-    @org.junit.Test
+public class AddTest {
+    @Test
     public void test_1_1(){
         Add add = new Add();
         Assert.assertEquals(add.add(1,1),2);
